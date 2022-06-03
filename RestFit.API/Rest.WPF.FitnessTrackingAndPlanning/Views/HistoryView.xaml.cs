02a8f06d56fc3e52df7748f413772778320bc726
Melanie Klein <klein@dial.de>
2022-06-03 12:06:40 +0200

    HealthData added to History

--- RestFit.API/Rest.WPF.FitnessTrackingAndPlanning/Views/HistoryView.xaml.cs
+++ RestFit.API/Rest.WPF.FitnessTrackingAndPlanning/Views/HistoryView.xaml.cs
@@ -16,10 +16,12 @@ public sealed partial class HistoryView : UserControl
 {
     #region Properties
 
-    public SeriesCollection? SeriesCollectionExercisesSets { get; set; }
-    public SeriesCollection? SeriesCollectionExercisesReps { get; set; }
-    public SeriesCollection? SeriesCollectionExercisesWeight { get; set; }
-    public List<string>? Labels { get; set; }
+    public SeriesCollection? SeriesCollectionHealthData { get; }
+    public List<string>? HealthDataLabels { get; }
+    public SeriesCollection? SeriesCollectionExercisesSets { get; }
+    public SeriesCollection? SeriesCollectionExercisesReps { get; }
+    public SeriesCollection? SeriesCollectionExercisesWeight { get; }
+    public List<string>? ExerciseLabels { get; }
 
     #endregion
 
@@ -27,19 +29,84 @@ public sealed partial class HistoryView : UserControl
     {
         InitializeComponent();
 
+        SeriesCollectionHealthData = new SeriesCollection();
+
         SeriesCollectionExercisesSets = new SeriesCollection();
         SeriesCollectionExercisesReps = new SeriesCollection();
         SeriesCollectionExercisesWeight = new SeriesCollection();
-        Labels = new List<string>();
+        HealthDataLabels = new List<string>();
+        ExerciseLabels = new List<string>();
 
-        GetExercisesFromHistory();
+        GetHealthDataFromHistory().ConfigureAwait(false);
+        GetExercisesFromHistory().ConfigureAwait(false);
 
         DataContext = this;
     }
 
+    private async Task GetHealthDataFromHistory()
+    {
+        IList<HealthUnitDto> allHealthData;
+        try
+        {
+            allHealthData = await Kernel.ClientHub?.V1.HealthUnitClient.GetHealthUnitsAsync()!;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+
+        var lineSeriesHealthDataWeight = new LineSeries
+            { Title = "Gewicht", Values = new ChartValues<double>(), LineSmoothness = 0 };
+        var lineSeriesHealthDataArmSize = new LineSeries
+            { Title = "Armumfang", Values = new ChartValues<double>(), LineSmoothness = 0 };
+        var lineSeriesHealthDataWaistSize = new LineSeries
+            { Title = "Taillenumfang", Values = new ChartValues<double>(), LineSmoothness = 0 };
+        var lineSeriesHealthDataHipSize = new LineSeries
+            { Title = "Hüftumfang", Values = new ChartValues<double>(), LineSmoothness = 0 };
+        var lineSeriesHealthDataThighSize = new LineSeries
+            { Title = "Oberschenkelumfang", Values = new ChartValues<double>(), LineSmoothness = 0 };
+
+        foreach (HealthUnitDto healthData in allHealthData.OrderBy(elem => elem.DateUtc))
+        {
+            if ((lineSeriesHealthDataWeight.Values.Count == 0 && lineSeriesHealthDataArmSize.Values.Count == 0 &&
+                 lineSeriesHealthDataWaistSize.Values.Count == 0)
+                || (lineSeriesHealthDataHipSize.Values.Count == 0 && lineSeriesHealthDataThighSize.Values.Count == 0))
+            {
+                if (HealthDataLabels != null)
+                {
+                    for (int i = 0;
+                         i < HealthDataLabels.IndexOf(healthData.DateUtc.Date.ToString("dd/MM/yyyy"));
+                         i++)
+                    {
+                        lineSeriesHealthDataWeight.Values.Add(0.0);
+                        lineSeriesHealthDataArmSize.Values.Add(0.0);
+                        lineSeriesHealthDataWaistSize.Values.Add(0.0);
+                        lineSeriesHealthDataHipSize.Values.Add(0.0);
+                        lineSeriesHealthDataThighSize.Values.Add(0.0);
+                    }
+                }
+            }
+
+            HealthDataLabels?.Add(healthData.DateUtc.Date.ToString("dd/MM/yyyy"));
+
+            lineSeriesHealthDataWeight.Values.Add(healthData.Weight);
+            lineSeriesHealthDataArmSize.Values.Add(healthData.ArmSize);
+            lineSeriesHealthDataWaistSize.Values.Add(healthData.WaistSize);
+            lineSeriesHealthDataHipSize.Values.Add(healthData.HipSize);
+            lineSeriesHealthDataThighSize.Values.Add(healthData.ThightSize);
+        }
+
+        SeriesCollectionHealthData?.Add(lineSeriesHealthDataWeight);
+        SeriesCollectionHealthData?.Add(lineSeriesHealthDataArmSize);
+        SeriesCollectionHealthData?.Add(lineSeriesHealthDataWaistSize);
+        SeriesCollectionHealthData?.Add(lineSeriesHealthDataHipSize);
+        SeriesCollectionHealthData?.Add(lineSeriesHealthDataThighSize);
+    }
+
     private async Task GetExercisesFromHistory()
     {
-        IList<UnitDto> allExercises = new List<UnitDto>();
+        IList<UnitDto> allExercises;
         try
         {
             allExercises = await Kernel.ClientHub?.V1.UnitClient.GetUnitsAsync()!;
@@ -53,22 +120,25 @@ public sealed partial class HistoryView : UserControl
         foreach (Tuple<string, List<UnitDto>> exercise in SortAndOrderExercises(allExercises))
         {
             var lineSeriesExerciseSets = new LineSeries
-                { Title = exercise.Item1, Values = new ChartValues<int>(), LineSmoothness = 0 };
+                { Title = exercise.Item1, Values = new ChartValues<long>(), LineSmoothness = 0 };
             var lineSeriesExerciseReps = new LineSeries
-                { Title = exercise.Item1, Values = new ChartValues<int>(), LineSmoothness = 0 };
+                { Title = exercise.Item1, Values = new ChartValues<long>(), LineSmoothness = 0 };
             var lineSeriesExerciseWeight = new LineSeries
                 { Title = exercise.Item1, Values = new ChartValues<double>(), LineSmoothness = 0 };
 
             foreach (UnitDto exerciseData in exercise.Item2)
             {
-                if (lineSeriesExerciseSets.Values.Count == 0 && lineSeriesExerciseReps.Values.Count == 0 && lineSeriesExerciseWeight.Values.Count == 0)
+                if (lineSeriesExerciseSets.Values.Count == 0 && lineSeriesExerciseReps.Values.Count == 0 &&
+                    lineSeriesExerciseWeight.Values.Count == 0)
                 {
-                    if (Labels != null)
+                    if (ExerciseLabels != null)
                     {
-                        for (int i = 0; i < Labels.IndexOf(exerciseData.DateUtc.Date.ToString("dd/MM/yyyy")); i++)
+                        for (int i = 0;
+                             i < ExerciseLabels.IndexOf(exerciseData.DateUtc.Date.ToString("dd/MM/yyyy"));
+                             i++)
                         {
-                            lineSeriesExerciseSets.Values.Add(0);
-                            lineSeriesExerciseReps.Values.Add(0); 
+                            lineSeriesExerciseSets.Values.Add((long)0);
+                            lineSeriesExerciseReps.Values.Add((long)0);
                             lineSeriesExerciseWeight.Values.Add(0.0);
                         }
                     }
@@ -101,9 +171,9 @@ public sealed partial class HistoryView : UserControl
                 sortedExercises.Add(new Tuple<string, List<UnitDto>>(unit.Type, new List<UnitDto> { unit }));
             }
 
-            if (Labels != null && !Labels.Contains(unit.DateUtc.Date.ToString("dd/MM/yyyy")))
+            if (ExerciseLabels != null && !ExerciseLabels.Contains(unit.DateUtc.Date.ToString("dd/MM/yyyy")))
             {
-                Labels.Add(unit.DateUtc.Date.ToString("dd/MM/yyyy"));
+                ExerciseLabels.Add(unit.DateUtc.Date.ToString("dd/MM/yyyy"));
             }
         }
 
