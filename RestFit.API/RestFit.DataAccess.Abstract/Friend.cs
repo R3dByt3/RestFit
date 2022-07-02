c947707e56966e90376bb5ba25229a585c524076
Marvin Gerdel <marvin.gerdel@web.de>
2022-07-02 17:22:35 +0200

    Fit.graphml und Friend.cs aktualisiert

--- RestFit.API/RestFit.DataAccess.Abstract/Friend.cs
+++ RestFit.API/RestFit.DataAccess.Abstract/Friend.cs
@@ -4,7 +4,7 @@
     {
         public string Id { get; set; } = string.Empty;
         public double AverageSets { get; set; } = 0;
-        public double AverageRepitions { get; set; } = 0;
+        public double AverageRepetitions { get; set; } = 0;
         public double AverageWeight { get; set; } = 0;
         public string Name { get; set; } = string.Empty;
         public string UserId { get; set; } = string.Empty;
